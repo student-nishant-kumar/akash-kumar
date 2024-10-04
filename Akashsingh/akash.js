62b8ae40867f064c61f64780a7973cd69afe3272
student-nishant-kumar <nishantkumar89116@gmail.com>
2024-10-04 12:21:04 +0000

    datatype

---
+++ Akashsingh/akash.js
@@ -0,0 +1,20 @@
+const accountId =  2356767
+let accountEmailId = "akashsinghh@gmail.com"
+var accountPassword = "234567"
+accountCity = "Bihar"
+let = accountstate; "Bengular"
+
+//accountId = 6 // not allowed
+
+accountEmailId = "aksdfh@gmail.com"
+accountPassword =  "325782356"
+accountCity = "Delhi"
+
+
+
+console.log(accountId);
+
+
+/* Prefer not to use var because of issue in block scope and functional scope */
+
+console.log([ accountId, accountPassword, accountCity, accountEmailId]);
---
+++ Akashsingh/datatype.js
@@ -0,0 +1,31 @@
+"use Strict"; // treat all Js code as near version
+
+
+// alert(3 + 3) we are using nodejs, not browser
+// And wrond code is not process run java script
+
+console.log(3 + 5) 
+console.log("akash kumer singh")
+
+let name = "ShimarSigh"
+let age = "34"
+let isLoggedIn  = "false"
+let isLoggedOff = "ture"
+let state;
+
+
+
+//number => 2 to power 54 
+// bright  
+// string =>""
+// boolean => true/false
+// null => standalone value
+// understand => 
+// symbol => unique
+
+// object 
+
+console.log(typeof "type of undefined"); //undefined
+console.log(typeof "type of null"); //object
+
+
