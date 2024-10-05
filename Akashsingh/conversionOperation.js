4abc4ed6e4989335c7094067daa487fe5d14e522
student-nishant-kumar <nishantkumar89116@gmail.com>
2024-10-05 06:15:04 +0000

    conversionOperation of datatype

---
+++ Akashsingh/conversionOperation.js
@@ -0,0 +1,49 @@
+
+// all type of conversion 
+
+// let score = 55
+
+// let score = "55sghfdfcjh"
+
+// let score = null;
+
+// let score = undefined;
+
+// let score = true;
+
+let score = "Akash"
+
+
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+
+// "55" => 55
+// "33abas" => NaN
+// true => 1; false = 0
+
+
+// let IsLoggedIn = 1
+// let IsLoggedIn = ""
+// let IsLoggedIn = "AKASH SINGH"
+ 
+
+
+// let booleanIsLoggedIn = Boolean(IsLoggedIn)
+// console.log(booleanIsLoggedIn) 
+
+// 1 => true; 0 => false
+// "" => false
+//  "AKASH SINGH" => ture
+
+let someNumber = 77
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
